8fb55ce8f9583cc7cef7540ff8fbe9501820b7c4
Shinsuke Sugaya <shinsuke@yahoo.co.jp>
2014-12-12 09:30:05 +0900

    update test case

--- src/test/java/org/codelibs/elasticsearch/synonym/SynonymPluginTest.java
+++ src/test/java/org/codelibs/elasticsearch/synonym/SynonymPluginTest.java
@@ -27,113 +27,147 @@ import org.junit.Test;
 
 public class SynonymPluginTest {
 
-    private ElasticsearchClusterRunner runner;
+	private ElasticsearchClusterRunner runner;
 
-    private File[] synonymFiles;
+	private File[] synonymFiles;
 
-    private int numOfNode = 1;
+	private int numOfNode = 1;
 
-    @Before
-    public void setUp() throws Exception {
-        runner = new ElasticsearchClusterRunner();
-        runner.onBuild(new ElasticsearchClusterRunner.Builder() {
-            @Override
-            public void build(final int number, final Builder settingsBuilder) {
-                settingsBuilder.put("http.cors.enabled", true);
-                settingsBuilder.put("index.number_of_replicas", 0);
-            }
-        }).build(newConfigs().ramIndexStore().numOfNode(numOfNode));
+	private int numOfDocs = 1000;
 
-        synonymFiles = new File[numOfNode];
-        for (int i = 0; i < numOfNode; i++) {
-            String confPath = runner.getNode(i).settings().get("path.conf");
-            synonymFiles[i] = new File(confPath, "synonym.txt");
-            updateDictionary(synonymFiles[i],
-                    "あい,かき,さしす,たちつて,なにぬねの\nうえお,くけこ,すせ");
-        }
+	@Before
+	public void setUp() throws Exception {
+		runner = new ElasticsearchClusterRunner();
+		runner.onBuild(new ElasticsearchClusterRunner.Builder() {
+			@Override
+			public void build(final int number, final Builder settingsBuilder) {
+				settingsBuilder.put("http.cors.enabled", true);
+				settingsBuilder.put("index.number_of_replicas", 0);
+				settingsBuilder.put("index.number_of_shards", 1);
+			}
+		}).build(newConfigs().ramIndexStore().numOfNode(numOfNode));
 
-        runner.ensureYellow();
-    }
+		synonymFiles = new File[numOfNode];
+		for (int i = 0; i < numOfNode; i++) {
+			String confPath = runner.getNode(i).settings().get("path.conf");
+			synonymFiles[i] = new File(confPath, "synonym.txt");
+			updateDictionary(synonymFiles[i], "あ,かき,さしす,たちつて,なにぬねの\n東京,とうきょう");
+		}
 
-    private void updateDictionary(File file, String content)
-            throws IOException, UnsupportedEncodingException,
-            FileNotFoundException {
-        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
-                new FileOutputStream(file), "UTF-8"))) {
-            bw.write(content);
-            bw.flush();
-        }
-    }
+		runner.ensureYellow();
+	}
 
-    @After
-    public void cleanUp() throws Exception {
-        runner.close();
-        runner.clean();
-    }
+	private void updateDictionary(File file, String content)
+			throws IOException, UnsupportedEncodingException,
+			FileNotFoundException {
+		try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(
+				new FileOutputStream(file), "UTF-8"))) {
+			bw.write(content);
+			bw.flush();
+		}
+	}
 
-    @Test
-    public void test_runCluster() throws Exception {
-        final String index = "dataset";
-        final String type = "item";
+	@After
+	public void cleanUp() throws Exception {
+		runner.close();
+		runner.clean();
+	}
 
-        final String indexSettings = "{\"index\":{\"analysis\":{"
-                + "\"tokenizer\":{"//
-                + "\"ngramSynonym\":{\"type\":\"ngram_synonym\",\"n\":\"2\",\"synonyms_path\":\"synonym.txt\"}"
-                + "},"//
-                + "\"analyzer\":{"
-                + "\"ngram_synonym_analyzer\":{\"type\":\"custom\",\"tokenizer\":\"ngramSynonym\"}"
-                + "}"//
-                + "}}}";
-        runner.createIndex(index,
-                ImmutableSettings.builder().loadFromSource(indexSettings)
-                        .build());
+	@Test
+	public void test_runCluster() throws Exception {
+		final String index = "dataset";
+		final String type = "item";
 
-        // create a mapping
-        final XContentBuilder mappingBuilder = XContentFactory.jsonBuilder()//
-                .startObject()//
-                .startObject(type)//
-                .startObject("properties")//
+		final String indexSettings = "{\"index\":{\"analysis\":{"
+				+ "\"tokenizer\":{"//
+				+ "\"2gram\":{\"type\":\"nGram\",\"min_gram\":\"2\",\"max_gram\":\"2\",\"token_chars\":[\"letter\",\"digit\"]},"
+				+ "\"2gram_synonym\":{\"type\":\"ngram_synonym\",\"n\":\"2\",\"synonyms_path\":\"synonym.txt\"}"
+				+ "},"//
+				+ "\"filter\":{"//
+				+ "\"synonym\":{\"type\":\"synonym\",\"synonyms_path\":\"synonym.txt\"}"
+				+ "},"//
+				+ "\"analyzer\":{"
+				+ "\"2gram_analyzer\":{\"type\":\"custom\",\"tokenizer\":\"2gram\",\"filter\":[\"synonym\"]},"
+				+ "\"2gram_synonym_analyzer\":{\"type\":\"custom\",\"tokenizer\":\"2gram_synonym\"}"
+				+ "}"//
+				+ "}}}";
+		runner.createIndex(index,
+				ImmutableSettings.builder().loadFromSource(indexSettings)
+						.build());
 
-                // id
-                .startObject("id")//
-                .field("type", "string")//
-                .field("index", "not_analyzed")//
-                .endObject()//
+		// create a mapping
+		final XContentBuilder mappingBuilder = XContentFactory.jsonBuilder()//
+				.startObject()//
+				.startObject(type)//
+				.startObject("properties")//
 
-                // msg1
-                .startObject("msg1")//
-                .field("type", "string")//
-                .field("analyzer", "ngram_synonym_analyzer")//
-                .endObject()//
+				// id
+				.startObject("id")//
+				.field("type", "string")//
+				.field("index", "not_analyzed")//
+				.endObject()//
 
-                .endObject()//
-                .endObject()//
-                .endObject();
-        runner.createMapping(index, type, mappingBuilder);
+				// msg1
+				.startObject("msg1")//
+				.field("type", "string")//
+				.field("analyzer", "2gram_synonym_analyzer")//
+				.endObject()//
 
-        int numOfDocs = 1000;
-        for (int i = 1; i <= numOfDocs; i++) {
-            final IndexResponse indexResponse1 = runner.insert(index, type,
-                    String.valueOf(i),
-                    "{\"msg1\":\"あいうえお\", \"msg2\":\"あいうえお\", \"id\":\"" + i
-                            + "\"}");
-            assertTrue(indexResponse1.isCreated());
-        }
-        runner.refresh();
+				// msg2
+				.startObject("msg2")//
+				.field("type", "string")//
+				.field("analyzer", "2gram_analyzer")//
+				.endObject()//
 
-        // 1000ドキュメント作成されたか確認
-        {
-            final SearchResponse searchResponse = runner.search(index, type,
-                    null, null, 0, 10);
-            assertEquals(numOfDocs, searchResponse.getHits().getTotalHits());
-        }
+				.endObject()//
+				.endObject()//
+				.endObject();
+		runner.createMapping(index, type, mappingBuilder);
 
-        {
-            final SearchResponse searchResponse = runner.search(index, type,
-                    QueryBuilders.matchQuery("msg1", "あい").type(Type.PHRASE),
-                    null, 0, 1000);
-            assertEquals(numOfDocs, searchResponse.getHits().getTotalHits());
-        }
+		for (int i = 1; i <= numOfDocs; i++) {
+			final IndexResponse indexResponse1 = runner.insert(index, type,
+					String.valueOf(i),
+					"{\"msg1\":\"あいうえお\", \"msg2\":\"あいうえお\", \"id\":\"" + i
+							+ "\"}");
+			assertTrue(indexResponse1.isCreated());
+		}
+		runner.refresh();
 
-    }
+		// 1000ドキュメント作成されたか確認
+		{
+			final SearchResponse searchResponse = runner.search(index, type,
+					null, null, 0, 10);
+			assertEquals(numOfDocs, searchResponse.getHits().getTotalHits());
+		}
+
+		assertDocCount(numOfDocs, index, type, "msg1", "あ");
+		assertDocCount(numOfDocs, index, type, "msg1", "あい");
+		assertDocCount(numOfDocs, index, type, "msg1", "あいう");
+		assertDocCount(numOfDocs, index, type, "msg1", "あいうえ");
+		assertDocCount(numOfDocs, index, type, "msg1", "あいうえお");
+
+		assertDocCount(0, index, type, "msg1", "かいうえお");
+		assertDocCount(numOfDocs, index, type, "msg1", "かきいうえお");
+		assertDocCount(numOfDocs, index, type, "msg1", "かきいうえ");
+		assertDocCount(numOfDocs, index, type, "msg1", "かきいう");
+		assertDocCount(numOfDocs, index, type, "msg1", "かきい");
+		assertDocCount(numOfDocs, index, type, "msg1", "かき");
+		assertDocCount(0, index, type, "msg1", "か");
+
+		assertDocCount(0, index, type, "msg2", "あ");
+		assertDocCount(numOfDocs, index, type, "msg2", "あい");
+		assertDocCount(numOfDocs, index, type, "msg2", "あいう");
+		assertDocCount(numOfDocs, index, type, "msg2", "あいうえ");
+		assertDocCount(numOfDocs, index, type, "msg2", "あいうえお");
+		assertDocCount(0, index, type, "msg2", "か");
+		assertDocCount(0, index, type, "msg2", "かき");
+	}
+
+	private void assertDocCount(int expected, final String index,
+			final String type, final String field, final String value) {
+		final SearchResponse searchResponse = runner.search(index, type,
+				QueryBuilders.matchQuery(field, value).type(Type.PHRASE), null,
+				0, numOfDocs);
+		assertEquals(expected, searchResponse.getHits().getTotalHits());
+	}
 }
